5846a1ebf5a06e7200ca808491c65d082ee2b9ce
Alpha <alphagma@gmail.com>
2017-02-19 20:50:15 -0500

    Recursively returning an organization's repository pages
    Fixes #1

--- server.js
+++ server.js
@@ -4,6 +4,10 @@
 
 	var reporter = require('./lib/core/bootstrap')();
 
-	reporter.sendOpenedPullRequestsEmail();
+	reporter.sendOpenedPullRequestsEmail().then(function() {
+		console.log('Finished without errors.');
+	}).catch(function(err) {
+		console.error(err);
+	});
 
 })();
